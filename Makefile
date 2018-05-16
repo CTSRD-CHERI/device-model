48d2d7fa63422b7783480bf3344e5e6ee7cdbcd1
Ruslan Bukin <br@bsdpad.com>
2018-05-16 16:52:08 +0100

    Initial import

---
+++ Makefile
@@ -0,0 +1,39 @@
+APP =		device-model
+ARCH =		mips
+
+CC =		${CROSS_COMPILE}gcc
+LD =		${CROSS_COMPILE}ld
+OBJCOPY =	${CROSS_COMPILE}objcopy
+
+LDSCRIPT =	${.CURDIR}/ldscript
+
+OBJECTS =	device-model.o				\
+		osfive/sys/dev/altera/jtag_uart.o	\
+		osfive/sys/mips/beri/beripic.o		\
+		osfive/sys/mips/mips/timer.o		\
+		osfive/sys/kern/subr_prf.o		\
+		osfive/sys/kern/subr_console.o		\
+		osfive/lib/libc/stdio/printf.o		\
+		osfive/lib/libc/string/bzero.o		\
+		osfive/lib/libc/string/strlen.o		\
+		start.o
+
+CFLAGS =	-O -pipe -g -nostdinc -fno-pic -mno-abicalls -G0	\
+	-march=mips64 -mabi=64 -msoft-float -ffreestanding -fwrapv	\
+	-gdwarf-2 -fno-common -fms-extensions -finline-limit=8000	\
+	-std=iso9899:1999 -fno-builtin-printf				\
+	-Wall -Wredundant-decls -Wnested-externs			\
+	-Wstrict-prototypes -Wmissing-prototypes -Wpointer-arith	\
+	-Winline -Wcast-qual -Wundef -Wno-pointer-sign			\
+	-fformat-extensions -Wmissing-include-dirs			\
+	-fdiagnostics-show-option -Wno-unknown-pragmas			\
+	-Wno-uninitialized -Werror
+
+all:	compile link binary
+
+clean:
+	rm -f ${OBJECTS:M*} ${APP}.*
+
+.include "${.CURDIR}/osfive/mk/user.mk"
+.include "${.CURDIR}/osfive/mk/compile.mk"
+.include "${.CURDIR}/osfive/mk/link.mk"
---
+++ device-model.c
@@ -0,0 +1,114 @@
+/*-
+ * Copyright (c) 2018 Ruslan Bukin <br@bsdpad.com>
+ * All rights reserved.
+ *
+ * This software was developed by SRI International and the University of
+ * Cambridge Computer Laboratory under DARPA/AFRL contract FA8750-10-C-0237
+ * ("CTSRD"), as part of the DARPA CRASH research programme.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#include <sys/cdefs.h>
+#include <sys/console.h>
+#include <sys/endian.h>
+#include <sys/systm.h>
+
+#include <mips/mips/timer.h>
+#include <mips/beri/beripic.h>
+#include <dev/altera/jtag_uart.h>
+
+#include <machine/cpufunc.h>
+#include <machine/cpuregs.h>
+
+#include "device-model.h"
+
+struct beripic_resource beripic1_res = {
+	.cfg = BERIPIC1_CFG,
+	.ip_read = BERIPIC1_IP_READ,
+	.ip_set = BERIPIC1_IP_SET,
+	.ip_clear = BERIPIC1_IP_CLEAR,
+};
+
+static struct aju_softc aju_sc;
+static struct beripic_softc beripic_sc;
+static struct mips_timer_softc timer_sc;
+
+static void
+uart_putchar(int c, void *arg)
+{
+	struct aju_softc *sc;
+
+	sc = arg;
+
+	if (c == '\n')
+		aju_putc(sc, '\r');
+
+	aju_putc(sc, c);
+}
+
+void
+udelay(uint32_t usec)
+{
+
+	mips_timer_udelay(&timer_sc, usec);
+}
+
+int
+main(void)
+{
+	uint64_t *addr;
+	uint32_t status;
+
+	/* Debug */
+	addr = (uint64_t *)0xffffffffb0800000;
+	*addr = 0x1515151515161617;
+
+	aju_init(&aju_sc, AJU1_BASE);
+
+	console_register(uart_putchar, (void *)&aju_sc);
+
+	beripic_init(&beripic_sc, &beripic1_res);
+	beripic_enable(&beripic_sc, 16, 0);
+
+	status = mips_rd_status();
+	status |= ((1 << 2) << 8);
+	status |= MIPS_SR_INT_IE;
+	status &= ~MIPS_SR_BEV;
+	mips_wr_status(status);
+
+	mips_timer_init(&timer_sc, MIPS_DEFAULT_FREQ);
+
+	while (1) {
+		printf("Hello World!\n");
+		udelay(1000000);
+	}
+
+	return (0);
+}
+
+void
+exception(void)
+{
+
+	printf("Exception!\n");
+}
