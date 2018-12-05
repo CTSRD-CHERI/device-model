418446c668c9e2bdf3e42f2965ff50e25d296d68
Ruslan Bukin <br@bsdpad.com>
2018-12-05 15:30:46 +0000

    cleanup: remove commented-out pthread calls

--- bhyve/block_if.c
+++ bhyve/block_if.c
@@ -29,9 +29,6 @@
  */
 
 #include <sys/cdefs.h>
-#if 0
-__FBSDID("$FreeBSD$");
-#endif
 
 #include <sys/param.h>
 #ifndef WITHOUT_CAPSICUM
@@ -40,25 +37,11 @@ __FBSDID("$FreeBSD$");
 #include <sys/queue.h>
 #include <sys/errno.h>
 #include <sys/stat.h>
-#if 0
-#include <sys/ioctl.h>
-#include <sys/disk.h>
-#endif
 
 #include <assert.h>
-#if 0
-#include <err.h>
-#include <fcntl.h>
-#endif
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#if 0
-#include <pthread.h>
-#include <pthread_np.h>
-#include <signal.h>
-#include <sysexits.h>
-#endif
 #include <unistd.h>
 
 #include <machine/atomic.h>
@@ -68,9 +51,6 @@ __FBSDID("$FreeBSD$");
 #include <limits.h>
 #include "ahci.h"
 #include "bhyve_support.h"
-#if 0
-#include "bhyverun.h"
-#endif
 #include "mevent.h"
 #include "block_if.h"
 
@@ -115,17 +95,12 @@ struct blockif_elem {
 	struct blockif_req  *be_req;
 	enum blockop	     be_op;
 	enum blockstat	     be_status;
-#if 0
-	pthread_t            be_tid;
-#else
 	int                  be_tid;
-#endif
 	off_t		     be_block;
 };
 
 struct blockif_ctxt {
 	int			bc_magic;
-	int			bc_fd;
 	int			bc_ischr;
 	int			bc_isgeom;
 	int			bc_candelete;
@@ -136,11 +111,6 @@ struct blockif_ctxt {
 	int			bc_psectsz;
 	int			bc_psectoff;
 	int			bc_closing;
-#if 0
-	pthread_t		bc_btid[BLOCKIF_NUMTHR];
-	pthread_mutex_t		bc_mtx;
-	pthread_cond_t		bc_cond;
-#endif
 
 	/* Request elements and free/pending/busy queues */
 	TAILQ_HEAD(, blockif_elem) bc_freeq;       
@@ -149,15 +119,7 @@ struct blockif_ctxt {
 	struct blockif_elem	bc_reqs[BLOCKIF_MAXREQ];
 };
 
-#if 0
-static pthread_once_t blockif_once = PTHREAD_ONCE_INIT;
-#endif
-
 struct blockif_sig_elem {
-#if 0
-	pthread_mutex_t			bse_mtx;
-	pthread_cond_t			bse_cond;
-#endif
 	int				bse_pending;
 	struct blockif_sig_elem		*bse_next;
 };
@@ -211,11 +173,7 @@ blockif_enqueue(struct blockif_ctxt *bc, struct blockif_req *breq,
 }
 
 static int
-#if 0
-blockif_dequeue(struct blockif_ctxt *bc, pthread_t t, struct blockif_elem **bep)
-#else
 blockif_dequeue(struct blockif_ctxt *bc, int t, struct blockif_elem **bep)
-#endif
 {
 	struct blockif_elem *be;
 
@@ -315,45 +273,6 @@ blockif_proc(struct blockif_ctxt *bc, struct blockif_elem *be, uint8_t *buf)
 	(*br->br_callback)(br, err);
 }
 
-#if 0
-static void *
-blockif_thr(void *arg)
-{
-	struct blockif_ctxt *bc;
-	struct blockif_elem *be;
-	pthread_t t;
-	uint8_t *buf;
-
-	bc = arg;
-	if (bc->bc_isgeom)
-		buf = malloc(MAXPHYS);
-	else
-		buf = NULL;
-	t = pthread_self();
-
-	pthread_mutex_lock(&bc->bc_mtx);
-	for (;;) {
-		while (blockif_dequeue(bc, t, &be)) {
-			pthread_mutex_unlock(&bc->bc_mtx);
-			blockif_proc(bc, be, buf);
-			pthread_mutex_lock(&bc->bc_mtx);
-			blockif_complete(bc, be);
-		}
-		/* Check ctxt status here to see if exit requested */
-		if (bc->bc_closing)
-			break;
-		pthread_cond_wait(&bc->bc_cond, &bc->bc_mtx);
-	}
-	pthread_mutex_unlock(&bc->bc_mtx);
-
-	if (buf)
-		free(buf);
-	pthread_exit(NULL);
-	return (NULL);
-}
-
-#else
-
 void *
 blockif_thr(void)
 {
@@ -380,35 +299,6 @@ blockif_thr(void)
 	return (NULL);
 }
 
-#endif
-
-#if 0
-static void
-blockif_sigcont_handler(int signal, enum ev_type type, void *arg)
-{
-	struct blockif_sig_elem *bse;
-
-	for (;;) {
-		/*
-		 * Process the entire list even if not intended for
-		 * this thread.
-		 */
-		do {
-			bse = blockif_bse_head;
-			if (bse == NULL)
-				return;
-		} while (!atomic_cmpset_ptr((uintptr_t *)&blockif_bse_head,
-					    (uintptr_t)bse,
-					    (uintptr_t)bse->bse_next));
-
-		pthread_mutex_lock(&bse->bse_mtx);
-		bse->bse_pending = 0;
-		pthread_cond_signal(&bse->bse_cond);
-		pthread_mutex_unlock(&bse->bse_mtx);
-	}
-}
-#endif
-
 static void
 blockif_init(void)
 {
@@ -420,32 +310,15 @@ blockif_init(void)
 	sc->buf = malloc(MAXPHYS);
 
 	blockif_sc = sc;
-#if 0
-	mevent_add(SIGCONT, EVF_SIGNAL, blockif_sigcont_handler, NULL);
-	(void) signal(SIGCONT, SIG_IGN);
-#endif
 }
 
 struct blockif_ctxt *
 blockif_open(const char *optstr, const char *ident)
 {
-#if 0
-	char tname[MAXCOMLEN + 1];
-	char name[MAXPATHLEN];
-#endif
-	char *nopt, *xopts, *cp;
 	struct blockif_ctxt *bc;
-#if 0
-	struct stat sbuf;
-	struct diocgattr_arg arg;
-#endif
 	off_t psectsz, psectoff;
-	int extra, fd, i, sectsz;
-#if 0
-	int nocache, sync, ro, candelete, geom, ssopt, pssopt;
-#else
+	int extra, i, sectsz;
 	int nocache, sync, ro, candelete, geom, ssopt;
-#endif
 #ifndef WITHOUT_CAPSICUM
 	cap_rights_t rights;
 	cap_ioctl_t cmds[] = { DIOCGFLUSH, DIOCGDELETE };
@@ -453,163 +326,30 @@ blockif_open(const char *optstr, const char *ident)
 
 	printf("%s: optstr %s ident %s\n", __func__, optstr, ident);
 
-#if 0
-	pthread_once(&blockif_once, blockif_init);
-#else
 	blockif_init();
-#endif
 
-	fd = -1;
 	ssopt = 0;
 	nocache = 0;
 	sync = 0;
 	ro = 0;
-
-	/*
-	 * The first element in the optstring is always a pathname.
-	 * Optional elements follow
-	 */
-#if 0
-	nopt = xopts = strdup(optstr);
-	while (xopts != NULL) {
-		cp = strsep(&xopts, ",");
-		if (cp == nopt)		/* file or device pathname */
-			continue;
-		else if (!strcmp(cp, "nocache"))
-			nocache = 1;
-		else if (!strcmp(cp, "sync") || !strcmp(cp, "direct"))
-			sync = 1;
-		else if (!strcmp(cp, "ro"))
-			ro = 1;
-		else if (sscanf(cp, "sectorsize=%d/%d", &ssopt, &pssopt) == 2)
-			;
-		else if (sscanf(cp, "sectorsize=%d", &ssopt) == 1)
-			pssopt = ssopt;
-		else {
-			printf("Invalid device option \"%s\"\n", cp);
-			goto err;
-		}
-	}
-#endif
-
 	extra = 0;
-#if 0
-	if (nocache)
-		extra |= O_DIRECT;
-	if (sync)
-		extra |= O_SYNC;
-#endif
 
-#if 0
-	fd = open(nopt, (ro ? O_RDONLY : O_RDWR) | extra);
-	if (fd < 0 && !ro) {
-		/* Attempt a r/w fail with a r/o open */
-		fd = open(nopt, O_RDONLY | extra);
-		ro = 1;
-	}
-
-	if (fd < 0) {
-		warn("Could not open backing file: %s", nopt);
-		goto err;
-	}
-
-        if (fstat(fd, &sbuf) < 0) {
-		warn("Could not stat backing file %s", nopt);
-		goto err;
-        }
-
-#ifndef WITHOUT_CAPSICUM
-	cap_rights_init(&rights, CAP_FSYNC, CAP_IOCTL, CAP_READ, CAP_SEEK,
-	    CAP_WRITE);
-	if (ro)
-		cap_rights_clear(&rights, CAP_FSYNC, CAP_WRITE);
-
-	if (cap_rights_limit(fd, &rights) == -1 && errno != ENOSYS)
-		errx(EX_OSERR, "Unable to apply rights for sandbox");
-#endif
-
-        /*
-	 * Deal with raw devices
-	 */
-        size = sbuf.st_size;
-	sectsz = DEV_BSIZE;
-	psectsz = psectoff = 0;
-	candelete = geom = 0;
-	if (S_ISCHR(sbuf.st_mode)) {
-		if (ioctl(fd, DIOCGMEDIASIZE, &size) < 0 ||
-		    ioctl(fd, DIOCGSECTORSIZE, &sectsz)) {
-			perror("Could not fetch dev blk/sector size");
-			goto err;
-		}
-		assert(size != 0);
-		assert(sectsz != 0);
-		if (ioctl(fd, DIOCGSTRIPESIZE, &psectsz) == 0 && psectsz > 0)
-			ioctl(fd, DIOCGSTRIPEOFFSET, &psectoff);
-		strlcpy(arg.name, "GEOM::candelete", sizeof(arg.name));
-		arg.len = sizeof(arg.value.i);
-		if (ioctl(fd, DIOCGATTR, &arg) == 0)
-			candelete = arg.value.i;
-		if (ioctl(fd, DIOCGPROVIDERNAME, name) == 0)
-			geom = 1;
-	} else
-		psectsz = sbuf.st_blksize;
-
-#ifndef WITHOUT_CAPSICUM
-	if (cap_ioctls_limit(fd, cmds, nitems(cmds)) == -1 && errno != ENOSYS)
-		errx(EX_OSERR, "Unable to apply rights for sandbox");
-#endif
-
-	if (ssopt != 0) {
-		if (!powerof2(ssopt) || !powerof2(pssopt) || ssopt < 512 ||
-		    ssopt > pssopt) {
-			printf("Invalid sector size %d/%d\n",
-			    ssopt, pssopt);
-			goto err;
-		}
-
-		/*
-		 * Some backend drivers (e.g. cd0, ada0) require that the I/O
-		 * size be a multiple of the device's sector size.
-		 *
-		 * Validate that the emulated sector size complies with this
-		 * requirement.
-		 */
-		if (S_ISCHR(sbuf.st_mode)) {
-			if (ssopt < sectsz || (ssopt % sectsz) != 0) {
-				printf("Sector size %d incompatible "
-				    "with underlying device sector size %d\n",
-				    ssopt, sectsz);
-				goto err;
-			}
-		}
-
-		sectsz = ssopt;
-		psectsz = pssopt;
-		psectoff = 0;
-	}
-#else
 	psectsz = 512;
 	psectoff = 0;
 	sectsz = 512;
 	ro = 0;
 	candelete = 0;
 	geom = 0;
-#endif
 
 	bc = calloc(1, sizeof(struct blockif_ctxt));
 	blockif_sc->bc = bc;
-#if 0
 	if (bc == NULL) {
-		perror("calloc");
-		goto err;
+		printf("%s: calloc failed\n", __func__);
+		return (NULL);
 	}
-#endif
 
 	bc->bc_magic = BLOCKIF_SIG;
-	bc->bc_fd = fd;
-#if 0
-	bc->bc_ischr = S_ISCHR(sbuf.st_mode);
-#endif
+	bc->bc_ischr = 0;
 	bc->bc_isgeom = geom;
 	bc->bc_candelete = candelete;
 	bc->bc_rdonly = ro;
@@ -618,10 +358,7 @@ blockif_open(const char *optstr, const char *ident)
 	bc->bc_sectsz = sectsz;
 	bc->bc_psectsz = psectsz;
 	bc->bc_psectoff = psectoff;
-#if 0
-	pthread_mutex_init(&bc->bc_mtx, NULL);
-	pthread_cond_init(&bc->bc_cond, NULL);
-#endif
+
 	TAILQ_INIT(&bc->bc_freeq);
 	TAILQ_INIT(&bc->bc_pendq);
 	TAILQ_INIT(&bc->bc_busyq);
@@ -630,24 +367,7 @@ blockif_open(const char *optstr, const char *ident)
 		TAILQ_INSERT_HEAD(&bc->bc_freeq, &bc->bc_reqs[i], be_link);
 	}
 
-#if 0
-	for (i = 0; i < BLOCKIF_NUMTHR; i++) {
-		pthread_create(&bc->bc_btid[i], NULL, blockif_thr, bc);
-		snprintf(tname, sizeof(tname), "blk-%s-%d", ident, i);
-		pthread_set_name_np(bc->bc_btid[i], tname);
-	}
-#endif
-
 	return (bc);
-#if 0
-err:
-	if (fd >= 0)
-		close(fd);
-#endif
-	free(cp);
-	free(xopts);
-	free(nopt);
-	return (NULL);
 }
 
 static int
@@ -660,21 +380,9 @@ blockif_request(struct blockif_ctxt *bc, struct blockif_req *breq,
 
 	err = 0;
 
-#if 0
-	pthread_mutex_lock(&bc->bc_mtx);
-#endif
-	if (!TAILQ_EMPTY(&bc->bc_freeq)) {
-		/*
-		 * Enqueue and inform the block i/o thread
-		 * that there is work available
-		 */
-#if 0
-		if (blockif_enqueue(bc, breq, op))
-			pthread_cond_signal(&bc->bc_cond);
-#else
+	if (!TAILQ_EMPTY(&bc->bc_freeq))
 		blockif_enqueue(bc, breq, op);
-#endif
-	} else {
+	else {
 		/*
 		 * Callers are not allowed to enqueue more than
 		 * the specified blockif queue limit. Return an
@@ -683,9 +391,6 @@ blockif_request(struct blockif_ctxt *bc, struct blockif_req *breq,
 		 */
 		err = E2BIG;
 	}
-#if 0
-	pthread_mutex_unlock(&bc->bc_mtx);
-#endif
 
 	return (err);
 }
@@ -739,9 +444,6 @@ blockif_cancel(struct blockif_ctxt *bc, struct blockif_req *breq)
 
 	assert(bc->bc_magic == BLOCKIF_SIG);
 
-#if 0
-	pthread_mutex_lock(&bc->bc_mtx);
-#endif
 	/*
 	 * Check pending requests.
 	 */
@@ -754,9 +456,6 @@ blockif_cancel(struct blockif_ctxt *bc, struct blockif_req *breq)
 		 * Found it.
 		 */
 		blockif_complete(bc, be);
-#if 0
-		pthread_mutex_unlock(&bc->bc_mtx);
-#endif
 
 		return (0);
 	}
@@ -772,9 +471,6 @@ blockif_cancel(struct blockif_ctxt *bc, struct blockif_req *breq)
 		/*
 		 * Didn't find it.
 		 */
-#if 0
-		pthread_mutex_unlock(&bc->bc_mtx);
-#endif
 		return (EINVAL);
 	}
 
@@ -785,11 +481,6 @@ blockif_cancel(struct blockif_ctxt *bc, struct blockif_req *breq)
 	while (be->be_status == BST_BUSY) {
 		struct blockif_sig_elem bse, *old_head;
 
-#if 0
-		pthread_mutex_init(&bse.bse_mtx, NULL);
-		pthread_cond_init(&bse.bse_cond, NULL);
-#endif
-
 		bse.bse_pending = 1;
 
 		do {
@@ -799,19 +490,7 @@ blockif_cancel(struct blockif_ctxt *bc, struct blockif_req *breq)
 					    (uintptr_t)old_head,
 					    (uintptr_t)&bse));
 
-#if 0
-		pthread_kill(be->be_tid, SIGCONT);
-
-		pthread_mutex_lock(&bse.bse_mtx);
-		while (bse.bse_pending)
-			pthread_cond_wait(&bse.bse_cond, &bse.bse_mtx);
-		pthread_mutex_unlock(&bse.bse_mtx);
-#endif
 	}
-
-#if 0
-	pthread_mutex_unlock(&bc->bc_mtx);
-#endif
 
 	/*
 	 * The processing thread has been interrupted.  Since it's not
@@ -823,27 +502,15 @@ blockif_cancel(struct blockif_ctxt *bc, struct blockif_req *breq)
 int
 blockif_close(struct blockif_ctxt *bc)
 {
-#if 0
-	void *jval;
-	int i;
-#endif
 
 	printf("%s\n", __func__);
+
 	assert(bc->bc_magic == BLOCKIF_SIG);
 
 	/*
 	 * Stop the block i/o thread
 	 */
-#if 0
-	pthread_mutex_lock(&bc->bc_mtx);
-#endif
 	bc->bc_closing = 1;
-#if 0
-	pthread_mutex_unlock(&bc->bc_mtx);
-	pthread_cond_broadcast(&bc->bc_cond);
-	for (i = 0; i < BLOCKIF_NUMTHR; i++)
-		pthread_join(bc->bc_btid[i], &jval);
-#endif
 
 	/* XXX Cancel queued i/o's ??? */
 
@@ -851,9 +518,6 @@ blockif_close(struct blockif_ctxt *bc)
 	 * Release resources
 	 */
 	bc->bc_magic = 0;
-#if 0
-	close(bc->bc_fd);
-#endif
 	free(bc);
 
 	return (0);
